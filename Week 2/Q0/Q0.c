c352b93d37ccfdc295639e23c7033e5da327e3f8
sreil <seanpreilly@gmail.com>
2015-06-29 06:21:07 -0600

    beginning of week 2

---
+++ Week 2/Q0/Q0.c
@@ -0,0 +1,24 @@
+/*********************************************************************
+** Program Filename: Q0
+** Author: Sean Reilly
+** Date: June 28, 2015
+** Description:  Print value of x
+** Input:
+** Output: Prints value of x
+*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void fooA(int *iptr)
+{
+    //add
+}
+
+int main()
+{
+    int x = 5; //declare x
+
+
+    return 0;
+}
