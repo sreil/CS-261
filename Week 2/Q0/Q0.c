02f59ccbb1541a7159d6f7a7223df612144c9101
sreil <seanpreilly@gmail.com>
2015-06-29 20:20:23 -0600

    finished Q0.c, started Q1.c

--- Week 2/Q0/Q0.c
+++ Week 2/Q0/Q0.c
@@ -1,24 +1,30 @@
 /*********************************************************************
-** Program Filename: Q0
+** Program Filename: Q0.c
 ** Author: Sean Reilly
 ** Date: June 28, 2015
 ** Description:  Print value of x
-** Input:
+** Input: none
 ** Output: Prints value of x
 *********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void fooA(int *iptr)
+void fooA(int *iptr) //In fooA(int * iptr), print the value of the integer pointed to by iptr, the address pointed to by iptr, and the address of iptr itself.
 {
-    //add
+    printf("The value of the integer pointed to by iptr is: %d \n", *iptr);
+    printf("The address pointed to by iptr is: %p \n", iptr);
+    printf("iptr's address is: %p \n", &iptr);
 }
 
 int main()
 {
-    int x = 5; //declare x
+    int x = 14;
+    printf("Address of x is: %p\n", &x); //In the main function, declare an integer, x. Print the address of x (using the address of operator).
 
+    fooA(&x); //Pass x as an argument to a function void fooA(int* iptr).
+
+    printf("Value of x is: %d\n", x); //In the main function, following the call to fooA(...), print the value of x.
 
     return 0;
 }
---
+++ Week 2/Q1/Q1.c
@@ -0,0 +1,23 @@
+/*********************************************************************
+** Program Filename: Q1.c
+** Author: Sean Reilly
+** Date: June 29, 2015
+** Description:  add
+** Input: none
+** Output: add
+*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student {
+    int id;
+    int score;
+};
+
+int main()
+{
+    struct student *st = 0;
+
+    return 0;
+}
