16456294abb7ee573b941d9d3b8f9e6788d52e0b
sreil <seanpreilly@gmail.com>
2015-07-03 06:46:36 -0600

    finished q4.c

--- Week 2/Q4/Q4.c
+++ Week 2/Q4/Q4.c
@@ -2,11 +2,10 @@
 ** Program Filename: Q4.c
 ** Author: Sean Reilly
 ** Date: July 1, 2015
-** Description: Modifies Q1
+** Description: Modifies Q1/Q3
 ** Input: none
-** Output: alternating upper and lowercase letters
+** Output: prints ID's and sorts scores in ascending order
 *********************************************************************/
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,32 +15,50 @@ struct student{
 	int score;
 };
 
-void sort(struct student* students, int n) // The function prototype is void sort(struct student* students, int n).
+void sort(struct student* students, int n)
 {
-    if(n == 0)
-        return;
+	int i, j;
+	struct student temp;
 
-    int i, j;
-    struct student temp;
-    for (i = n - 1; i > 0; i--)
-    {
-        for (j = 0; j < i; j++)
-        {
-            if (students[j].score > students[j + 1].score)
-            {
-                temp = students[j];
-                students[j] = students[j + 1];
-                students[j + 1] = temp;
-            }
-        }
-    }
+	for (i = 0; i< n - 1; i++)
+	{
+		for (j = 0; j < n - 1 - i; j++)
+		{
+			if (students[j + 1].score < students[j].score)     //Modify the above sort(...) function from Q.3 to sort an array of n students based on their scores in ascending order
+			{
+				temp = students[j];
+				students[j] = students[j + 1];
+				students[j + 1] = temp;
+			}
+		}
+	}
 }
-
 
 int main()
 {
+	int n = 20, i;  //used int of 20 again
 
-    struct student *students = malloc(10*(sizeof(struct student)));
+	struct student *students = malloc(sizeof(struct student)*n); //modified from Q3
+
+    srand(time(NULL)); //random numbers and ID's
+
+    for(i = 0; i<n; i++)
+    {
+        students[i].id = i+1;
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        students[i].score = rand()%101; //for scores, random goes to 100
+    }
+    sort(students, n);
+
+    printf("Sorted ID's and scores are as follows:\n");
+
+    for(i=0; i<n; i++)
+	{
+        printf("ID: %d \t\tScore: %d\n", students[i].id, students[i].score);
+    }
 
     return 0;
 }
